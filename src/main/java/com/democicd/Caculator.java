77631eb0926550d8bb168371ae7c0ddfe151807b
PTTin1952 <tinptps1952@gmail.com>
2025-04-09 20:03:15 +0700

    fail

--- src/main/java/com/democicd/Caculator.java
+++ src/main/java/com/democicd/Caculator.java
@@ -2,10 +2,10 @@ package com.democicd;
 
 public class Caculator implements Icaculator {
     public Integer sum(Integer a, Integer b) {
-        return a+b;
+        return 0;
     }
 
     public Integer sub(Integer a, Integer b) {
-        return a-b;
+        return 0;
     }
 }

384adf57235a164257bd0d6115823135694d28de
PTTin1952 <tinptps1952@gmail.com>
2025-04-09 14:29:50 +0700

    first commit

---
+++ src/main/java/com/democicd/Caculator.java
@@ -0,0 +1,11 @@
+package com.democicd;
+
+public class Caculator implements Icaculator {
+    public Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+
+    public Integer sub(Integer a, Integer b) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/democicd/Icaculator.java
@@ -0,0 +1,6 @@
+package com.democicd;
+
+public interface Icaculator {
+    public Integer sum(Integer a, Integer b);
+    public Integer sub(Integer a, Integer b);
+}

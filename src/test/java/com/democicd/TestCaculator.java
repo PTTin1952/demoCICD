7e85f7237481a8f6a0a8371c91d4aa16b222fa7b
PTTin1952 <tinptps1952@gmail.com>
2025-04-09 14:52:19 +0700

    test fail

---
+++ src/test/java/com/democicd/TestCaculator.java
@@ -0,0 +1,20 @@
+package com.democicd;
+
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
+public class TestCaculator {
+
+    @Test
+    public void testSum() {
+        Caculator caculator = new Caculator();
+        Assert.assertEquals(caculator.sum(2,3),5);
+
+    }
+    @Test
+    public void testSub() {
+        Caculator caculator = new Caculator();
+        Assert.assertEquals(caculator.sum(2,2),0);
+
+    }
+}
